624a214094fda165f25919623a5a46bb2a66d4e4
kevin_gerardo <kevingerardo23@gmail.com>
2023-05-29 16:54:42 -0500

    ITEM new page

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,6 +8,22 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(params.require(:item).permit(:name, :price, :stock))
+    if @item.save
+      flash[:notice] = "Item created succesfully"
+      redirect_to @item
+    else
+      flash[:alert] = 'There was something wrong the item info'
+      render 'new'
+    end
+  end
+
+
   
 end
   
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ApplicationRecord
   has_many :likes, dependent: :destroy
+  validates :name, presence: true,  length: {minimum: 6, maximum: 100}
 
 end
