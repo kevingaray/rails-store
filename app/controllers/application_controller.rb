f3693f961cf5cba9c61feb2f8181f161c96da254
kevin_gerardo <kevingerardo23@gmail.com>
2023-05-30 09:18:26 -0500

    USERS devise views and controller, added first and last name to forms

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def verify_is_admin
+    redirect_to(root_path) unless current_user&.admin
+  end
+
+  def verify_is_support
+    redirect_to(root_path) unless current_user&.support
+  end
+
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name])
+  end
 end
