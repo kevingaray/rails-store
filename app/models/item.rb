3a7a74c538147e3c9c7ca39e8b4a49874de3abb5
kevin_gerardo <kevingerardo23@gmail.com>
2023-05-30 08:43:14 -0500

    SEARCH functionality

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,12 @@ class Item < ApplicationRecord
   has_many :likes, dependent: :destroy
   validates :name, presence: true,  length: {minimum: 6, maximum: 100}
 
+  def self.search(search)
+    if search
+      @items = Item.where("LOWER(name) LIKE LOWER(?)", "%"+search+"%")
+    else
+      @items = Item.all
+    end
+  end
+
 end
