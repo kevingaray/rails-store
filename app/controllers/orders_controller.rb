ee9a40ae95392cc227f2f5f2a1f50950e06e8589
kevin_gerardo <kevingerardo23@gmail.com>
2023-06-01 08:34:36 -0500

    Add function reduce stock when order is created

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -19,14 +19,23 @@ class OrdersController < ApplicationController
 
   def create
     @order = Order.new(user_id: current_user.id)
-    @current_cart.line_items.each do |item|
-      @order.line_items << item
-      item.cart_id = nil
+    @current_cart.line_items.each do |li|
+      reduce_stock(li)
+      @order.line_items << li
+      li.cart_id = nil
     end
     @order.save
     Cart.destroy(session[:cart_id])
     session[:cart_id] = nil
     redirect_to root_path
+  end
+
+  private
+
+  def reduce_stock(line_item)
+    item = Item.find(line_item.item_id)
+    item.stock -= line_item.quantity
+    item.save
   end
   
 end
