a03dc750a1e94e44e95945f961b04a14fdd992e9
kevin_gerardo <kevingerardo23@gmail.com>
2023-06-05 18:38:41 -0500

    Item model minitest

--- test/models/item_test.rb
+++ test/models/item_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
+require 'faker'
 
 class ItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "item should not be valid without a name" do
+    item = Item.create(
+      price: rand(10.00..50.00),
+      stock: rand(20..50)
+    )
+    assert_not item.valid?
+  end
+
 end
