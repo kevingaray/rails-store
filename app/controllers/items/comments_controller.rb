ce44eef4a5263cbc881b0affc4ab63dbf2a3ea24
kevin_gerardo <kevingerardo23@gmail.com>
2023-06-08 16:57:56 -0500

    Refactor Approved and delete comment

--- app/controllers/items/comments_controller.rb
+++ app/controllers/items/comments_controller.rb
@@ -1,24 +1,27 @@
 class Items::CommentsController < CommentsController
   before_action :set_commentable
+  before_action :find_comment, only: %i[delete approved]
   before_action :verify_is_support, only: %i[delete approved]
 
   def delete
-    comment = Comment.find(params[:comment_id])
-    comment.destroy
-    redirect_to item_path(params[:item_id])
+    @comment.destroy
+    redirect_to item_path(@commentable)
   end
 
   def approved
-    comment = Comment.find(params[:comment_id])
-    comment.approved = true
-    comment.save
-    redirect_to item_path(params[:item_id])
+    @comment.approved = true
+    @comment.save
+    redirect_to item_path(@commentable)
   end
 
   private
 
   def set_commentable
     @commentable = Item.find(params[:item_id])
+  end
+
+  def find_comment
+    @comment = Comment.find(params[:comment_id])
   end
   
 end
