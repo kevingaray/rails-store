6ecd405a36624498145ec116caf419594d3a285a
kevin_gerardo <kevingerardo23@gmail.com>
2023-06-09 10:24:43 -0500

    Tests items controller: edit create delete

--- test/controllers/items_controller_test.rb
+++ test/controllers/items_controller_test.rb
@@ -3,18 +3,18 @@ require 'test_helper'
 class ItemsControllerTest < ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
 
-  test 'should get index' do
+  test 'no user can get to item index' do
     get items_path
     assert_response :success
   end
 
-  test 'should show item' do
+  test 'no user can get to show item' do
     item = FactoryBot.create(:item)
     get item_path(item)
     assert_response :success
   end
 
-  test 'should get new only to admin' do
+  test 'only admin can get to new item page' do
     admin = FactoryBot.create(:admin_user)
     sign_in(admin)
 
@@ -22,12 +22,63 @@ class ItemsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test 'should reject new to no admin' do
+  test 'no admin cant get to new item page' do
     user = FactoryBot.create(:user)
     sign_in(user)
 
     get new_item_url
     assert_response :redirect
   end
+
+  test 'admin can create item' do
+    admin = FactoryBot.create(:admin_user)
+    sign_in(admin)
+
+    assert_difference('Item.count') do
+      post items_url, params: { item: { name: 'Red tomate', price: 14, stock: 5, tags: ''  } }
+    end
+    assert_response :redirect
+  end
+
+  test "customer can't create item" do
+    user = FactoryBot.create(:user)
+    sign_in(user)
+
+    assert_difference('Item.count', 0) do
+      post items_url, params: { item: { name: 'Red tomate', price: 14, stock: 5, tags: ''  } }
+    end
+    assert_response :redirect
+  end
+
+  test 'admin should get edit' do
+    admin = FactoryBot.create(:admin_user)
+    sign_in(admin)
+    item = FactoryBot.create(:item)
+
+    get edit_item_path(item)
+    assert_response :success
+  end
+
+  test 'should destroy item' do
+    admin = FactoryBot.create(:admin_user)
+    sign_in(admin)
+    item = FactoryBot.create(:item)
+
+    assert_difference('Item.where(deleted_at: nil).count', -1) do
+      delete item_path(item)
+    end
+
+    assert_response :redirect
+  end
+
+  test 'admin update item' do
+    admin = FactoryBot.create(:admin_user)
+    sign_in(admin)
+    item = FactoryBot.create(:item)
+
+    patch item_url(item), params: { item: { name: 'Green tomate', price: 17, stock: 5, tags: 'blue' } }
+    assert_redirected_to item_url(item)
+  end
+
 
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,7 +3,7 @@ require_relative "../config/environment"
 require "rails/test_help"
 
 class ActiveSupport::TestCase
-  parallelize(workers: :number_of_processors, with: :threads)
+  # parallelize(workers: :number_of_processors, with: :threads)
   fixtures :all
   include FactoryBot::Syntax::Methods
 end
