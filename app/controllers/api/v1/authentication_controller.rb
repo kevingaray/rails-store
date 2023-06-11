072efb8ca23943da7d47fffde069a74a833de5d9
kevin_gerardo <kevingerardo23@gmail.com>
2023-06-11 16:24:54 -0500

    Show login endpoint : admin and support value

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -9,7 +9,7 @@ module Api
         @user = User.find_by_email(params[:email])
         if @user&.valid_password?(params[:password])
           token = jwt_encode(user_id: @user.id)
-          render json: { token: token }, status: :ok
+          render json: { token: token , admin: @user.admin , support: @user.support}, status: :ok
         else
           render json: { error: 'Invalid username or password' }, status: :unauthorized
         end
