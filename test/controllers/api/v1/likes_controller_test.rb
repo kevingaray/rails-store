08905e99f705ba55d93d3ea0e8cdccc3619255d9
kevin_gerardo <kevingerardo23@gmail.com>
2023-06-15 11:30:20 -0500

    Test like dislike api

---
+++ test/controllers/api/v1/likes_controller_test.rb
@@ -0,0 +1,41 @@
+require 'test_helper'
+
+class LikesControllerTest < ActionDispatch::IntegrationTest
+
+  test 'customer can CREATE like item' do
+    token = token_login_as_customer
+    id = FactoryBot.create(:item).id
+
+    post "/api/v1/items/#{id}/likes",
+         headers: { 'Authorization' => token }
+
+    assert_equal 1, JSON.parse(response.body)['data']['likes']
+    assert_response :success
+  end
+
+  test 'customer can DESTROY like item' do
+    token = token_login_as_customer
+    id = FactoryBot.create(:item).id
+
+    post "/api/v1/items/#{id}/likes",
+         headers: { 'Authorization' => token }
+
+    assert_equal 1, JSON.parse(response.body)['data']['likes']
+    assert_response :success
+
+    delete "/api/v1/items/#{id}/likes",
+           headers: { 'Authorization' => token }
+
+    assert_equal 0, JSON.parse(response.body)['data']['likes']
+    assert_response :success
+  end
+
+  private
+
+  def token_login_as_customer
+    user = FactoryBot.create(:user)
+    post '/api/v1/auth/login', params: { email: user.email, password: user.password }
+    JSON.parse(response.body)['token']
+  end
+
+end
