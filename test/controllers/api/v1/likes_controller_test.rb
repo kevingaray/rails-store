8178ae8dd2907d34cb952ee1676904445ede003d
kevin_gerardo <kevingerardo23@gmail.com>
2023-06-18 09:46:56 -0500

    Refactor api tests: likes and orders

--- test/controllers/api/v1/likes_controller_test.rb
+++ test/controllers/api/v1/likes_controller_test.rb
@@ -2,40 +2,33 @@ require 'test_helper'
 
 class LikesControllerTest < ActionDispatch::IntegrationTest
 
-  test 'customer can CREATE like item' do
-    token = token_login_as_customer
-    id = FactoryBot.create(:item).id
+  def setup
+    user = FactoryBot.create(:user)
+    post '/api/v1/auth/login', params: { email: user.email, password: user.password }
+    @token = JSON.parse(response.body)['token']
+    @item_id = FactoryBot.create(:item).id
+  end
 
-    post "/api/v1/items/#{id}/likes", headers: { 'Authorization' => token }
+  test 'customer can CREATE like item' do
+    post "/api/v1/items/#{@item_id}/likes", headers: { 'Authorization' => @token }
     perform_enqueued_jobs
-    get "/api/v1/items/#{id}"
+    get "/api/v1/items/#{@item_id}"
     assert_equal 1, JSON.parse(response.body)['data']['likes']
     assert_response :success
   end
 
   test 'customer can DESTROY like item' do
-    token = token_login_as_customer
-    id = FactoryBot.create(:item).id
-
-    post "/api/v1/items/#{id}/likes", headers: { 'Authorization' => token }
+    post "/api/v1/items/#{@item_id}/likes", headers: { 'Authorization' => @token }
     perform_enqueued_jobs
-    get "/api/v1/items/#{id}"
+    get "/api/v1/items/#{@item_id}"
     assert_equal 1, JSON.parse(response.body)['data']['likes']
     assert_response :success
 
-    delete "/api/v1/items/#{id}/likes", headers: { 'Authorization' => token }
+    delete "/api/v1/items/#{@item_id}/likes", headers: { 'Authorization' => @token }
     perform_enqueued_jobs
-    get "/api/v1/items/#{id}"
+    get "/api/v1/items/#{@item_id}"
     assert_equal 0, JSON.parse(response.body)['data']['likes']
     assert_response :success
-  end
-
-  private
-
-  def token_login_as_customer
-    user = FactoryBot.create(:user)
-    post '/api/v1/auth/login', params: { email: user.email, password: user.password }
-    JSON.parse(response.body)['token']
   end
 
 end
--- test/controllers/api/v1/orders_controller_test.rb
+++ test/controllers/api/v1/orders_controller_test.rb
@@ -2,50 +2,35 @@ require 'test_helper'
 
 class OrdersControllerTest < ActionDispatch::IntegrationTest
 
+  def setup
+    user = FactoryBot.create(:user)
+    post '/api/v1/auth/login', params: { email: user.email, password: user.password }
+    @token = JSON.parse(response.body)['token']
+    @item = FactoryBot.create(:item)
+  end
+
   test 'customer can see their orders' do
-    token = token_login_as_customer
-
-    get '/api/v1/orders',
-        headers: { 'Authorization' => token }
-
+    get '/api/v1/orders', headers: { 'Authorization' => @token }
     assert_response :success
   end
 
   test 'customer checkout and see order' do
-    token = token_login_as_customer
-    item = FactoryBot.create(:item)
-
     # added a cart
-    post '/api/v1/line_items',
-         headers: { 'Authorization' => token },
-         params: { line_item: { item_id: item.id, quantity: 5 } }
-
+    post '/api/v1/line_items', headers: { 'Authorization' => @token },
+      params: { line_item: { item_id: @item.id, quantity: 5 } }
     assert_response :success
 
     # checkout
-    post '/api/v1/orders',
-         headers: { 'Authorization' => token }
-
+    post '/api/v1/orders', headers: { 'Authorization' => @token }
     order_id = JSON.parse(response.body)['id']
-
     assert_response :success
 
     # see order
-    get "/api/v1/orders/#{order_id}",
-        headers: { 'Authorization' => token }
-    
-    assert_equal item.name, JSON.parse(response.body)['data'][0]['items'][0]['name']
+    get "/api/v1/orders/#{order_id}", headers: { 'Authorization' => @token }
+
+    assert_equal @item.name, JSON.parse(response.body)['data'][0]['items'][0]['name']
     assert_equal 5, JSON.parse(response.body)['data'][0]['items'][0]['quantity']
     assert_response :success
-
-  end
-
-  private
-
-  def token_login_as_customer
-    user = FactoryBot.create(:user)
-    post '/api/v1/auth/login', params: { email: user.email, password: user.password }
-    JSON.parse(response.body)['token']
   end
 
 end
