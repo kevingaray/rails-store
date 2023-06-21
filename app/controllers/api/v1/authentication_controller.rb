8fc28d1553a31305113b6d27220db984fd4c6a2c
kevin_gerardo <kevingerardo23@gmail.com>
2023-06-21 08:04:36 -0500

    Soft deleted user cannot get access token

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -7,7 +7,7 @@ module Api
       # #POST auth/login
       def login
         @user = User.find_by_email(params[:email])
-        if @user&.valid_password?(params[:password])
+        if @user&.valid_password?(params[:password]) && !@user.deleted_at
           token = jwt_encode(user_id: @user.id)
           render json: { token: token , admin: @user.admin , support: @user.support}, status: :ok
         else
